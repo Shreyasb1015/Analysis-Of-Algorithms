db212ceeed57738fd8df362f95bf2dc23f38b3c8
Shreyasb1015 <bagweshreyas1015@gmail.com>
2024-01-09 19:11:02 +0530

    recursion basics

--- recursion/sumofNnumbers.cpp
+++ recursion/sumofNnumbers.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 using namespace std;
 
-// Creating a function to calculate sum of n numbers
+// Creating a recursive function to calculate sum of n numbers
 int sum(int n){
 
     if(n==0){
@@ -14,5 +14,6 @@ int sum(int n){
 int main(){
 
     int num=10;
+    //calling the function
     cout<<"Sum of "<<num<<" numbers is "<<sum(num)<<endl;
 }
